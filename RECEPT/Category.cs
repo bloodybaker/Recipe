6b45b544438824ee7c1a7259ded6884900e45f8a
Yevhenii Chykalov <gamchanel@gmail.com>
2020-08-09 11:53:06 +0300

    Initial commit

---
+++ RECEPT/Category.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace RECEPT
+{
+    public class Category
+    {
+        public List<object> soup { get; set; } 
+        public List<object> meat { get; set; } 
+        public List<object> fish { get; set; } 
+        public List<object> salad { get; set; } 
+    }
+}
---
+++ RECEPT/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RECEPT
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
